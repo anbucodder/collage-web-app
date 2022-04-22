4be7ba8d2f4883341bc6d5042814ffa3788278fa
anbucodder <100592337+anbucodder@users.noreply.github.com>
2022-04-22 21:16:04 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,12 @@
+var SpeechRecognition = window.webkitSpeechRecognition;
+
+var recognition = new SpeechRecognition();
+
+function start(){
+    recognition.start();
+}
+recognition.onresult = function(event){
+    console.log(event);
+    var content=event.results[0][0].transcript;
+    console.log(content);
+}
