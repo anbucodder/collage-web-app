a1ca064dda99ed6189b8918101b9e644980dbbf3
anbucodder <100592337+anbucodder@users.noreply.github.com>
2022-04-27 07:25:38 +0530

    Update app.js

--- app.js
+++ app.js
@@ -9,4 +9,20 @@ recognition.onresult = function(event){
     console.log(event);
     var content=event.results[0][0].transcript;
     console.log(content);
+    localStorage.setItem("content",content)
 }
+function speak(){
+    var synth=window.speechSynthesis;
+    speak_data=localStorage.getItem("content")
+    var utterThis = new speechSynthesisUtterance(speak_data);
+    synth.speek(utterThis);
+    Webcam.attach(camera);
+}
+
+Webcam.set({
+    width:360,
+    height:650,
+    image_format:'png',
+    png_quality:90
+});
+camera = document.getElementById("camera");
